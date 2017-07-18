460d25169778f0c7df69025cba9d5dda853a717d
unknown <joane.artiaga@gmail.com>
2017-07-18 23:36:22 +0800

    (6) Implement day and night theme modes

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -5,19 +5,30 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
+    int styleId = R.style.AppTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null){
+            styleId = savedInstanceState.getInt("Theme");
+        }
+        setTheme(styleId);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState == null) {
+
+
+        if (savedInstanceState == null) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             ScoreFragment firstTeam = new ScoreFragment("Team 1", 0);
@@ -26,6 +37,39 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.add(R.id.fragment2_container, secondTeam, "Second Team");
             fragmentTransaction.commit();
         }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Theme", styleId);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.dayMode:
+                styleId = R.style.AppTheme;
+                recreate();
+                break;
+            case R.id.nightMode:
+                styleId = R.style.AppTheme_Night;
+                recreate();
+                break;
+        }
+        return true;
 
     }
 }
